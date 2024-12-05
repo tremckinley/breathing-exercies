ca21f2a0726d234f80b950d7868eedefefd42b75
Tremckinley <tremckinley901@gmail.com>
2024-12-05 11:17:18 -0600

    added timer option

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const startButton = document.getElementById("counter");
 const innerCircle = document.getElementById("inner-circle");
 const title = document.querySelector("h1");
 const dropDown = document.getElementById("breathing-exercise-selector");
-const selection = dropDown.value;
+const timeDropDown = document.getElementById("breathing-time-selector");
+let selection = dropDown.value;
+let selectedTime = timeDropDown.value;
 
 const breathingExercises = [
   {
     name: "Focus",
-    description: "&#127919;Follow along with the timer to focus breathe.&#127919;",
+    description:
+      "&#127919;Follow along with the timer to focus breathe.&#127919;",
     inhaleTimeInSeconds: 4,
     holdTimeOne: 4,
     exhaleTimeInSeconds: 4,
     holdTimeTwo: 4,
-    bgColor: "#7c3aed"
+    bgColor: "#7c3aed",
   },
   {
     name: "Relaxation",
@@ -21,7 +24,7 @@ const breathingExercises = [
     holdTimeOne: 7,
     exhaleTimeInSeconds: 8,
     holdTimeTwo: 0,
-    bgColor: "teal"
+    bgColor: "teal",
   },
   {
     name: "Energy Boost",
@@ -30,16 +33,17 @@ const breathingExercises = [
     holdTimeOne: 0,
     exhaleTimeInSeconds: 1,
     holdTimeTwo: 0,
-    bgColor: "darkorange"
+    bgColor: "darkorange",
   },
   {
     name: "Deep Calm",
-    description: "&#x1F9D8;&#x1F3FF;&#x200D;&#x2642;&#xFE0F;Enter a state of deep relaxation.&#x1F9D8;&#x1F3FF;&#x200D;&#x2642;&#xFE0F;",
+    description:
+      "&#x1F9D8;&#x1F3FF;&#x200D;&#x2642;&#xFE0F;Enter a state of deep relaxation.&#x1F9D8;&#x1F3FF;&#x200D;&#x2642;&#xFE0F;",
     inhaleTimeInSeconds: 6,
     holdTimeOne: 0,
     exhaleTimeInSeconds: 9,
     holdTimeTwo: 0,
-    bgColor: "midnightblue"
+    bgColor: "midnightblue",
   },
   {
     name: "Sleep",
@@ -48,23 +52,35 @@ const breathingExercises = [
     holdTimeOne: 7,
     exhaleTimeInSeconds: 9,
     holdTimeTwo: 0,
-    bgColor: "burlywood"
-  }
+    bgColor: "burlywood",
+  },
 ];
 
-let breathingStyle = breathingExercises.find(style => style.name.toLowerCase() === selection);
+let breathingStyle = breathingExercises.find(
+  (style) => style.name.toLowerCase() === selection
+);
 document.getElementById("directions").innerHTML = breathingStyle["description"];
-dropDown.addEventListener('change', function() {
-  const selectedStyle = dropDown.value;
-  breathingStyle = breathingExercises.find(style => style.name.toLowerCase() === selectedStyle.toLowerCase());
-  document.getElementById("directions").innerHTML = breathingStyle["description"];
-  document.querySelector(".outer-circle").style.backgroundColor = breathingStyle.bgColor;
-  console.log(breathingStyle)
-  
+
+//Event Listener to change style
+dropDown.addEventListener("change", function () {
+  selection = dropDown.value;
+  breathingStyle = breathingExercises.find(
+    (style) => style.name.toLowerCase() === selection.toLowerCase()
+  );
+  document.getElementById("directions").innerHTML =
+    breathingStyle["description"];
+  document.querySelector(".outer-circle").style.background =
+    breathingStyle.bgColor;
+  //console.log(breathingStyle)
 });
 
+//Event Listener to change time
+timeDropDown.addEventListener("change", function () {
+  //convert minutes to milliseconds
+  selectedTime = timeDropDown.value;
+  //console.log(selectedTime)
+});
 
-//need to fix inner circle transistion durations
 
 function fillBar(transTime) {
   innerCircle.style.transitionDuration = `${transTime}s`;
@@ -76,72 +92,84 @@ function emptyBar(transTime) {
   innerCircle.style.transitionDuration = `${transTime}s`;
   innerCircle.style.height = "6em";
   innerCircle.style.width = "6em";
-};
+}
 
-function activateTimer(style) {
-//function for calling repeated setTimeout functions. time is added to the same variable to mimmick a sleep function cleanly. seconds variable is multiplied by 1000 to simplify the argument.
+function activateTimer(exercise) {
+  //function for calling repeated setTimeout functions. time is added to the same variable to mimmick a sleep function cleanly. seconds variable is multiplied by 1000 to simplify the argument.
   let milliseconds = 1000;
   function nextLine(action, seconds) {
-  setTimeout(action, milliseconds);
-  milliseconds += seconds*1000;
-  };
+    setTimeout(action, milliseconds);
+    milliseconds += seconds * 1000;
+  }
 
-  console.log(style.inhaleTimeInSeconds, style.exhaleTimeInSeconds, style.holdTimeOne, style.holdTimeTwo)
+  let inhaleTimeInSeconds, exhaleTimeInSeconds, holdTimeOne, holdTimeTwo;
 
-  counter.innerHTML = "Let's Begin"
-    nextLine(() => counter.innerHTML = "3", 1);
-    nextLine(() => counter.innerHTML = "2", 1);
-    nextLine(() => counter.innerHTML = "1", 1);
-    
-    //FOR 3 rotations, do breathing exercise
-    //fill bar over 4 seconds
-    //hold
-    //empty bar over 4 seconds
-    //hold
-  for (let i=0; i<3; i++) {
+  inhaleTimeInSeconds = exercise.inhaleTimeInSeconds;
+  holdTimeOne = exercise.holdTimeOne;
+  exhaleTimeInSeconds = exercise.exhaleTimeInSeconds;
+  holdTimeTwo = exercise.holdTimeTwo;
+
+  /*console.log(
+    exercise.inhaleTimeInSeconds,
+    exercise.exhaleTimeInSeconds,
+    exercise.holdTimeOne,
+    exercise.holdTimeTwo
+  );*/
+
+  counter.innerHTML = "Let's Begin";
+  nextLine(() => (counter.innerHTML = "3"), 1);
+  nextLine(() => (counter.innerHTML = "2"), 1);
+  nextLine(() => (counter.innerHTML = "1"), 1);
+
+  //Until timer is up, do breathing exercise
+  //fill bar
+  //hold (optional)
+  //empty bar
+  //hold (optional)
+  while (milliseconds < selectedTime * 60000 + 3) {
     //Breathe In
     nextLine(() => {
-      counter.innerHTML = "Breathe In"
-      fillBar(style.inhaleTimeInSeconds)  
-    }, style.inhaleTimeInSeconds);
+      counter.innerHTML = "Breathe In";
+      fillBar(inhaleTimeInSeconds);
+      //console.log(milliseconds)
+    }, inhaleTimeInSeconds);
 
     //First Hold
-    
-      nextLine(() => {
-        counter.innerHTML = "Hold" 
-      }, style.holdTimeOne);
-    
+
+    nextLine(() => {
+      counter.innerHTML = "Hold";
+    }, holdTimeOne);
 
     //Breath Out
     nextLine(() => {
-      counter.innerHTML = "Breathe Out"
-      emptyBar(style.exhaleTimeInSeconds)  
-    }, style.exhaleTimeInSeconds);
+      counter.innerHTML = "Breathe Out";
+      emptyBar(exhaleTimeInSeconds);
+    }, exhaleTimeInSeconds);
 
     //Second Hold
-      nextLine(() => {
-        counter.innerHTML = "Hold" 
-      }, style.holdTimeTwo);
+    nextLine(() => {
+      counter.innerHTML = "Hold";
+    }, holdTimeTwo);
   }
   //End with a compliment
-  nextLine(() => counter.innerHTML = "Great Job", 1);
-  dropDown.style.pointerEvents = "auto"
-  dropDown.style.appearance = "auto"
+  nextLine(() => (counter.innerHTML = "Great Job"), 1);
+  nextLine(() => {
+    dropDown.style.pointerEvents = "all";
+    dropDown.style.appearance = "";
+  }, 1);
 }
-
-
 
 // activate on button click
 startButton.onclick = () => {
-    if (counter.innerHTML == "Start" || counter.innerHTML == "Great Job") {
-      console.log("breathing:", breathingStyle)
-      activateTimer(breathingStyle);
-      dropDown.style.pointerEvents = "none"
-      dropDown.style.appearance = "none"
-    };
-  };
+  if (counter.innerHTML == "Start" || counter.innerHTML == "Great Job") {
+    //console.log("breathing:", breathingStyle);
+    activateTimer(breathingStyle);
+    dropDown.style.pointerEvents = "none";
+    dropDown.style.appearance = "none";
+  }
+};
 
 //restart button
-document.getElementById('restart').onclick = () => {
+document.getElementById("restart").onclick = () => {
   location.reload();
-}
+};
